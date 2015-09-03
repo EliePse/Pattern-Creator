bb3686ef7d20053fac4ebf0cdaea02fc5b05479b
Eliepse <eliepse.meignan@gmail.com>
2015-09-03 17:50:26 +0200

    Temporary possibility to save as png

--- script.js
+++ script.js
@@ -134,6 +134,18 @@ $(function() {
 		
 	});
 	
+	var button = document.getElementById('btn-download');
+	button.addEventListener('click', function (e) {
+		var dataURL = canvas.toDataURL('image/png');
+		button.href = dataURL;
+	});
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
